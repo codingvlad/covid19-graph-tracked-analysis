a843e0ab87692cf2ea9a771e2e378abb4ccab016
codingvlad <vald.filip.dc@gmail.com>
2020-03-22 04:19:24 +0100

    Show different colors for postal codes based on number of connections

--- frontend/src/app/navigation/pages/hello/map-view.component.ts
+++ frontend/src/app/navigation/pages/hello/map-view.component.ts
@@ -5,6 +5,7 @@ import { Inject } from '@angular/core';
 import { MAT_SNACK_BAR_DATA } from '@angular/material';
 import * as L from 'leaflet';
 import * as d3 from 'd3';
+import { GraphService } from 'src/app/services/graph.service';
 
 export interface PostalCodeMapInfo {
   name: string;
@@ -25,82 +26,99 @@ export class MapViewComponent implements AfterViewInit {
 
   map: any;
   munichLatLng = L.latLng(48.1351, 11.5820);
+  postalCodeData: any;
 
   constructor(
     private dataService: DataService,
+    private graphService: GraphService,
     private userFeedback: MatSnackBar) { }
 
   ngAfterViewInit() {
-    this.initMap();
-    this.loadPostalCodes();
+    this.loadPostalCodeData().then(data => {
+      const randomPostalCodesToHighlight = this.graphService.initGraph(data);
+      this.initMap(data, randomPostalCodesToHighlight);
+    })
+    this.userFeedback.openFromComponent(UserActionFeedbackComponent, {
+      duration: 7000,
+      data: "This tool display FAKE DATA for demo purposes!!!"
+    });
   }
 
-  private initMap(): void {
+  private loadPostalCodeData = () => d3.json('assets/data/plz-5stellig.geojson')
 
-    d3.json('assets/data/plz-5stellig.geojson').then((data: any) => {
-
-      this.map = L.map('mapView', {
-        center: this.munichLatLng,
-        zoom: 12
-      });
-
-      const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 14,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-      });
-
-      tiles.addTo(this.map);
-
-      //initialize svg to add to map
-      L.svg({ clickable: true }).addTo(this.map) // we have to make the svg layer clickable 
-
-      //Create selection using D3
-      const overlay = d3.select(this.map.getPanes().overlayPane)
-      const svg = overlay.select('svg').attr('pointer-events', 'auto')
-      // create a group that is hidden during zooming
-      const g = svg.append('g').attr('class', 'leaflet-zoom-hide')
-
-      const _map = this.map;
-      // Use Leaflets projection API for drawing svg path (creates a stream of projected points)
-      const projectPoint = function (x, y) {
-        const point = _map.latLngToLayerPoint(new L.LatLng(y, x))
-        this.stream.point(point.x, point.y)
-      }
-
-      // Use d3's custom geo transform method to implement the above
-      const projection = d3.geoTransform({ point: projectPoint })
-      // creates geopath from projected points (SVG)
-      const pathCreator = d3.geoPath().projection(projection)
-
-      //const mapping: Array<PostalCodeMapInfo> = data.features.map(feature => this.extractPostalCodeInfo(feature));
-      const areaPaths = g.selectAll('path')
-        .data(data.features)
-        .join('path')
-        .attr('fill-opacity', 0)
-        .attr('stroke', 'black')
-        .style('stroke-dasharray', '5, 5')
-        .attr('z-index', 3000)
-        .attr('stroke-width', 1)
-        .on('mouseover', (_, i, g) => {
-          d3.select(g[i])
-            .attr('fill', 'red')
-            .attr('fill-opacity', 0.4)
-        })
-        .on('mouseout', (_, i, g) => {
-          d3.select(g[i])
-            .attr('fill', 'black')
-            .attr('fill-opacity', 0)
-        })
-;
-
-      // Function to place svg based on zoom
-      const onZoom = () => areaPaths.attr('d', pathCreator);
-      // initialize positioning
-      onZoom();
-      // reset whenever map is moved
-      this.map.on('zoomend', onZoom);
+  private initMap(data: any, randomPostalCodesToHighlight: Array<string>): void {
+    this.map = L.map('mapView', {
+      center: this.munichLatLng,
+      zoom: 12
     });
 
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 14,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+
+    //initialize svg to add to map
+    L.svg({ clickable: true }).addTo(this.map) // we have to make the svg layer clickable 
+
+    //Create selection using D3
+    const overlay = d3.select(this.map.getPanes().overlayPane)
+    const svg = overlay.select('svg').attr('pointer-events', 'auto')
+    // create a group that is hidden during zooming
+    const g = svg.append('g').attr('class', 'leaflet-zoom-hide')
+
+    const _map = this.map;
+    // Use Leaflets projection API for drawing svg path (creates a stream of projected points)
+    const projectPoint = function (x, y) {
+      const point = _map.latLngToLayerPoint(new L.LatLng(y, x))
+      this.stream.point(point.x, point.y)
+    }
+
+    // Use d3's custom geo transform method to implement the above
+    const projection = d3.geoTransform({ point: projectPoint })
+    // creates geopath from projected points (SVG)
+    const pathCreator = d3.geoPath().projection(projection)
+
+    const color = d3.scaleLinear<string>().domain([1, 5])
+      .range(["yellow", "red"]);
+
+    const colorOfRandomPostalCodes = (feature: any) => {
+      if (randomPostalCodesToHighlight.includes(feature.properties.plz)) {
+        const postalCodeConnectivity = Math.floor(Math.random() * 5) + 1
+        return color(postalCodeConnectivity);
+      }
+      else {
+        return 'green';
+      }
+    }
+
+    //const mapping: Array<PostalCodeMapInfo> = data.features.map(feature => this.extractPostalCodeInfo(feature));
+    const areaPaths = g.selectAll('path')
+      .data(data.features)
+      .join('path')
+      .attr('fill-opacity', 0.3)
+      .attr('fill', colorOfRandomPostalCodes)
+      .attr('stroke', 'black')
+      .style('stroke-dasharray', '5, 5')
+      .attr('z-index', 3000)
+      .attr('stroke-width', 1)
+      .on('mouseover', (_, i, g) => {
+        d3.select(g[i])
+          .attr('fill-opacity', 0.5)
+      })
+      .on('mouseout', (_, i, g) => {
+        d3.select(g[i])
+          .attr('fill-opacity', 0.3)
+      })
+      ;
+
+    // Function to place svg based on zoom
+    const onZoom = () => areaPaths.attr('d', pathCreator);
+    // initialize positioning
+    onZoom();
+    // reset whenever map is moved
+    this.map.on('zoomend', onZoom);
   }
 
 
---
+++ frontend/src/app/services/graph.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GraphService {
+
+  liveGraph: any;
+  workGraph: any;
+  studyGraph: any;
+  replenishGraph: any;
+  entertainGraph: any;
+
+  postalCodes: any;
+
+  constructor() { }
+
+  initGraph(postalCodesData: any): Array<string> {
+    console.log(postalCodesData.features.length)
+    
+    return this.shuffle(postalCodesData.features
+      .map(f => f.properties.plz.toString()))
+      .slice(0, 3000);
+  }
+
+  shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+}
